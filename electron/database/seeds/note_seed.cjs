3c34dd95b407194fdb2db074af1a623d2afcfc43
Mailan Franco <francocreepypasta@gmail.com>
2025-05-30 09:05:21 -0300

    Create note_seed.cjs

---
+++ electron/database/seeds/note_seed.cjs
@@ -0,0 +1,32 @@
+const { faker } = require('@faker-js/faker');
+const path = require('node:path')
+require('dotenv').config({ path: path.resolve(__dirname, '../../../.env') });
+
+exports.seed = async function (knex) {
+
+  // console.log(process.env.KNEX_NOTE_USER_EMAIL_TO_SEED)
+  // const teste = await knex('users').where('email', process.env.KNEX_NOTE_USER_EMAIL_TO_SEED).first()
+  // console.log(teste.id)
+  if (process.env.KNEX_NOTE_DELETE_ALL === 'true') {
+    await knex('notes').del();
+  }
+
+  let response = await knex('users').where('email', process.env.KNEX_NOTE_USER_EMAIL_TO_SEED).first();
+  if (!response) {
+    response = await knex('users').select('*').first();
+  }
+  const user = response;
+  // console.log(user)
+
+  const notes = [];
+
+  for (let i = 0; i < 10; i++) {
+    notes.push({
+      title: faker.lorem.words(3),
+      content: faker.lorem.words(30),
+      user_id: user.id || await (knex('users').first()).id
+    });
+  }
+
+  await knex('notes').insert(notes);
+};

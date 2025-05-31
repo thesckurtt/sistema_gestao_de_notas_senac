54eac2c663b3dba100ac8cd6d738b16c18c3f09e
Mailan Franco <francocreepypasta@gmail.com>
2025-05-31 11:33:22 -0300

    Update NotesFacade.cjs

--- electron/database/facades/NotesFacade.cjs
+++ electron/database/facades/NotesFacade.cjs
@@ -1,12 +1,48 @@
 const db = require('../knex.cjs')
 
 class NotesFacade {
-  async getAllNotes({ user_id }) {
+  static errorHandler(error) {
+    return { error: true, message: `[NotesFacade]: ${error.message || 'Unexpected error'}` }
+  }
+
+  static async getAllNotes({ user_id }) {
     try {
       const notes = await db('notes').where('user_id', user_id).orderBy('created_at', 'desc')
       return notes || []
-    }  catch (error) {
-      return { error: true, message: `[NotesFacade]: ${error.message || 'Unexpected error'}` }
+    } catch (error) {
+      return this.errorHandler(error)
     }
   }
+
+  static async deleteNote({ note_id, user_id }) {
+    try {
+      const result = await db('notes').where({ id: note_id, user_id }).del()
+      console.log('result: ', result)
+      if (result === 1) {
+        return { error: false, message: 'Note deleted successfully' }
+      }
+      return { error: true, message: 'Note not found or already deleted' }
+    } catch (error) {
+      return this.errorHandler(error)
+    }
+  }
+
+  static async createNote({ user_id, title, content }) {
+    try {
+      const [newNote] = await db('notes').insert({ user_id, title, content })
+      if (newNote) {
+        return { error: false, message: 'Note created successfully', note_id: newNote }
+      }
+      return { error: true, message: 'Note not created' }
+    } catch (error) {
+      return this.errorHandler(error)
+    }
+  }
+
 }
+
+// NotesFacade.getAllNotes({ user_id: 53 }).then((notes) => console.log(notes[0].title))
+// NotesFacade.deleteNote({ note_id: 12, user_id: 53 }).then((result) => console.log(result))
+// (db('notes').insert({ user_id: 53, title: "teste", content: "Lorem ipsum dolor it" }).returning('*')).then((result) => console.log(result))
+module.exports = NotesFacade
+

8d5004dba621c324dd756a8baed6dfb858f0cfe9
Mailan Franco <francocreepypasta@gmail.com>
2025-05-31 14:57:28 -0300

    Create AsideNotesDashboard.jsx

---
+++ src/components/AsideNotesDashboard.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useAuth } from "../context/AuthContext";
+
+export const AsideNotesDashboard = ({ notes, setIsNewNote, setNote }) => {
+  const { user } = useAuth();
+  return (
+    <aside
+      className="bg-dark py-4"
+      style={{
+        overflowY: "scroll",
+        maxHeight: "calc(100vh - 50px)",
+        width: "330px",
+      }}
+    >
+      {notes ? (
+        notes.map((note) => {
+          return (
+            <div
+              onClick={() => {
+                setIsNewNote(false);
+                setNote({
+                  title: note.title,
+                  content: note.content,
+                  user_id: user.id,
+                  id: note.id,
+                });
+              }}
+              key={note.id}
+              className="card c-pointer d-flex justify-content-center card-body aside-card mb-2"
+            >
+              <h5 className="card-title">
+                Nota: <span className="fw-light">{note.title}</span>
+              </h5>
+              <p style={{ fontSize: "0.8em", margin: "0" }}>
+                {note.content.length > 60
+                  ? note.content.substring(0, 60) + "..."
+                  : note.content}
+              </p>
+            </div>
+          );
+        })
+      ) : (
+        <p>nenhuma nota</p>
+      )}
+    </aside>
+  );
+};

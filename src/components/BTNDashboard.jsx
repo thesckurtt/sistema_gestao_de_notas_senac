208c2f775ff828193fb7d54d867776896f6c29d6
Mailan Franco <francocreepypasta@gmail.com>
2025-06-02 08:56:05 -0300

    Update BTNDashboard.jsx

--- src/components/BTNDashboard.jsx
+++ src/components/BTNDashboard.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
 
-export const BTNDashboard = ({customClass, label, action}) => {
+export const BTNDashboard = ({ customClass, label, ...attrs }) => {
   return (
-    <button className={`btn ${customClass}`} onClick={action}>{label}</button>
-  )
-}
+    <button className={`btn ${customClass}`} {...attrs}>
+      {label}
+    </button>
+  );
+};
